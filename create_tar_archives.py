4e8cc13facc9562ebe2412285cc99ce89c8d4d63
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2017-09-14 10:03:53 +0200

    added doc and better code style

--- create_tar_archives.py
+++ create_tar_archives.py
@@ -3,22 +3,54 @@ import os
 import tarfile
 
 def walk(folder):
+    """
+    Returns the contents of folder: directories and files.
+    """
     all_files = [os.path.join(folder, dir_) for dir_ in os.listdir(folder)]
     dirs = filter(os.path.isdir, all_files)
     files = filter(os.path.isfile, all_files)
     return dirs, files
 
-
 def is_pyecloud_sim_folder(folder):
+    """
+    Checks if a folder has the contents that identify it as a PyECLOUD simulation folder.
+    """
     dirs, files = walk(folder)
     base_files = map(os.path.basename, files)
     base_dirs = map(os.path.basename, dirs)
-    output = ('simulations' in base_dirs and 'config' in base_dirs and
+    is_sim_folder = ('simulations' in base_dirs and 'config' in base_dirs and
               ('run' in base_files or 'run_htcondor' in base_files))
-    return output
+    return is_sim_folder
 
+def make_tar_archive(source, tarname):
+    """
+    Does nothing if tarname already exists
+    """
+    target_folder = os.path.dirname(tarname)
+    source_parent = os.path.dirname(source)
+    source_base = os.path.basename(source)
+
+    if os.path.isfile(tarname):
+        print('%s already exists.' % tarname)
+        return
+
+    if not os.path.isdir(target_folder):
+        os.makedirs(target_folder)
+
+    print('Creating %s from %s' % (tarname, source))
+    with tarfile.open(tarname, 'w') as tar:
+        old_dir = os.getcwd()
+        try:
+            os.chdir(source_parent)
+            tar.add(source_base)
+        finally:
+            os.chdir(old_dir)
+    print('Done')
 
 def recursively_make_tar_archives(source_folder, target_folder, only_verbose):
+    """
+    run with only_verbose=True to see what it is doing.
+    """
 
     dirs, _ = walk(source_folder)
 
@@ -29,13 +61,7 @@ def recursively_make_tar_archives(source_folder, target_folder, only_verbose):
             if only_verbose:
                 print('I would create %s from %s' % (tarname, subdir))
             else:
-                if not os.path.isdir(target_folder):
-                    os.makedirs(target_folder)
-                if os.path.isfile(tarname):
-                    print('%s already exists.' % tarname)
-                else:
-                    with tarfile.open(tarname, 'w') as tar:
-                        tar.add(subdir)
+                make_tar_archive(subdir, tarname)
         else:
             new_target_folder = os.path.join(target_folder, base)
             recursively_make_tar_archives(subdir, new_target_folder, only_verbose)
@@ -49,4 +75,7 @@ if __name__ == '__main__':
     parser.add_argument('--real', action='store_true')
     args = parser.parse_args()
 
-    recursively_make_tar_archives(args.source_folder, args.target_folder, not args.real)
+    only_verbose = not args.real
+
+    recursively_make_tar_archives(args.source_folder, args.target_folder, only_verbose)
+
